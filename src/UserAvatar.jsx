1c491d2f06c4207ae7fc85711b453d99033ce7aa
Sameer <k200439@nu.edu.pk>
2022-11-26 04:42:04 +0500

    user avatar styling was done

--- src/UserAvatar.jsx
+++ src/UserAvatar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./UserAvatar.css"
 function UserAvatar(props) {
-    return <div>
+    return <div className="userAvatar">
         <img src={props.src} className={props.className} alt={props.alt} />
-        <h6 className="Rank">{props.rank}</h6>
-        <p>{props.userName}</p>
+        <h3 className="name">{props.userName}</h3>
+        <span className="rank">{props.rank}</span>
     </div>
 }
 
