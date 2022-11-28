3ebce686eca6783bd643af45f0ba09449890212c
Sameer <k200439@nu.edu.pk>
2022-11-28 11:08:11 +0500

    changed the button position on book page

--- src/components/BookPage.jsx
+++ src/components/BookPage.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import BookCover from "./BookCover";
 import ChooseSection from "./ChooseSection";
 import Reviews from "./Reviews.jsx";
@@ -9,26 +9,31 @@ import Rating from '@mui/material/Rating';
 function BookPage(props) {
     let [Selected, setSelected] = useState("about");
 
-    function SelectAbout(){
+    function SelectAbout() {
         setSelected("about");
     }
 
-    function SelectDetails(){
+    function SelectDetails() {
         setSelected("details");
     }
 
 
 
     return <div>
-        <div className="whiteBackground"/>
+        <div className="whiteBackground" />
         <div className="bookPage">
             <h3>{props.book.name}</h3>
             <div className="bookInfo">
                 <BookCover src={props.book.src} alt={props.book.alt} className="large" />
                 <UserAvatar src={props.user.src} userName={props.user.name} subheading={props.user.title} className="mainUserImage" />
-                <Rating value={props.book.rating} readOnly/>
+                <Rating value={props.book.rating} readOnly />
+                <form action="" className="addBookButton">
+                    <button type="submit"value="Request Book" className="button-primary button-full- button-medium ">Request Book</button>
+                    <input type="hidden" name="email" value={props.user.email} />
+                    <input type="hidden" name="email" value={props.book.name} />
+                </form>
             </div>
-            <ChooseSection selected={Selected} SelectAbout={SelectAbout} SelectDetails={SelectDetails}/>
+            <ChooseSection selected={Selected} SelectAbout={SelectAbout} SelectDetails={SelectDetails} />
             <div className="content">
                 {Selected === "about" && <p>{props.book.about}</p>}
 
@@ -38,16 +43,12 @@ function BookPage(props) {
                         <p><b>Condition: </b>{props.details.condition}</p>
                         <p><b>Additional Details: </b>{props.details.additonalDetails}</p>
                         <h3>Reviews: </h3>
-                        {props.book.reviews.map(review => <Reviews key={review.key} reviewer={review.reviewer} date={review.date} content={review.content} rating = {review.rating} />)}
+                        {props.book.reviews.map(review => <Reviews key={review.key} reviewer={review.reviewer} date={review.date} content={review.content} rating={review.rating} />)}
                     </div>
                 }
 
             </div>
-            <form action="">
-                <button type="submit" className="button-primary button-full-width button-long-text ">Request Book</button>
-                <input type="hidden" name="email" value={props.user.email}/>
-                <input type="hidden" name="email" value={props.book.name}/>
-            </form>
+
         </div>
     </div>
 }
