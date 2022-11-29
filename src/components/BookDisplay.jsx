e4bbab5c1e09ed6ab7288b8b0329b6921364f4cc
AminFarjad <amin.farjad.1999@gmail.com>
2022-11-29 19:03:51 +0500

    on click function added

--- src/components/BookDisplay.jsx
+++ src/components/BookDisplay.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import "./styles/bookCover.css";
 
-function BookCover(props) {
+function BookDisplay(props) {
     return <div>
         <img src={props.src} alt={props.alt} className={props.className + " bookCover"} onClick={props.onpress}/>
     </div>
 }
 
 
-export default BookCover;
+export default BookDisplay;
--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import BookCover from "./BookCover";
+import BookDisplay from "./BookDisplay";
 import "./ListBooks.css"
 
 var obje=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+function click(e){
+    // console.log(e.target.src);
+    // return <BookDisplay />
+
+}
 function ListBooks(props) {
+    const [display,set]=useState(false);
+    const [image,setimage] =useState("");
+    // const []
+
     return (
         <>
 
 
      <div  className={props.value?"Flex ":"Flex wrap"}>
      {obje.map((val,val1)=>{
-       return <BookCover src={"./testCover.jpg"} className={"small details"}   />
+       return <BookCover src={props.image} className={"small details"}  onpress={click} />
     })}
 
-        {props.value1?<BookCover src={"add.png"} className={"small details"} />: ""}
+        {props.value1?<BookCover src={"add.png"} className={"small details"} onpress={click}/>: ""}
      </div>
         </>
 
